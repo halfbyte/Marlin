f22ab910da81771c56a285567778742af5adfab4
Jan Krutisch <jan@krutisch.de>
2020-01-13 09:47:25 +0100

    fix a couple of configs, baking TMC current

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1057,15 +1057,15 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 202
-#define Y_BED_SIZE 202
+#define X_BED_SIZE 201
+#define Y_BED_SIZE 201
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -26
 #define Y_MIN_POS -1
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS 215
+#define Y_MAX_POS 205
 #define Z_MAX_POS 240
 
 /**
@@ -1094,7 +1094,7 @@
 #endif
 
 #if EITHER(MIN_SOFTWARE_ENDSTOPS, MAX_SOFTWARE_ENDSTOPS)
-  //#define SOFT_ENDSTOPS_MENU_ITEM  // Enable/Disable software endstops from the LCD
+  #define SOFT_ENDSTOPS_MENU_ITEM  // Enable/Disable software endstops from the LCD
 #endif
 
 /**
